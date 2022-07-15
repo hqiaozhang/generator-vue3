733102ed5597c46e6553bab00144e3861cb629d2
jodyzhq <991034150@qq.com>
2022-07-15 17:59:00 +0800

    修改serve

---
+++ build/webpack.common.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const config = require('./base.config')
+const utils = require('./utils')
+// 最新的 vue-loader 中，VueLoaderPlugin 插件的位置有所改变
+const { VueLoaderPlugin } = require("vue-loader");
+
+/**
+ * @param {string} dir 目录 
+ */
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  context: path.resolve(__dirname, '../'),
+  // 出口
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json', '.css', '.scss'],
+    alias: {
+      // vue: 'vue/index.js',
+      '@': resolve('src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },  
+      {
+        test: /\.(png|jpe?g|gif|svg|.ico)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('images/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(), // vue-loader插件
+  ],
+};
